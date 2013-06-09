da400e2ccf5086323faebc2ef261822749fc1217
Guillermo Correa <luis.guillermo.correa@hotmail.com>
2013-06-09 12:28:38 -0500

    Added Quartz reference

---
+++ Worker/ScheduledJob.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Worker
+{
+    public class ScheduledJob : IScheduledJob
+    {
+        public void Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
